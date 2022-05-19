5edfb9d4862511e338643e8c4e8a686437bcc79a
Shovon018 <sadikuzzaman1996@gmail.com>
2022-05-19 17:22:01 +0600

    db connection info updated

--- relaxy-api/src/app.module.ts
+++ relaxy-api/src/app.module.ts
@@ -14,34 +14,20 @@ const envFilePath = [`env/${!ENV ? `.env` : `.env.${ENV}`}`];
       isGlobal: true,
       envFilePath,
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        // host: configService.get(ConfigureEnum.DATABASE_HOST),
-        // port: +configService.get(ConfigureEnum.DATABASE_PORT),
-        // username: configService.get(ConfigureEnum.DATABASE_USER),
-        // password: configService.get(ConfigureEnum.DATABASE_PASSWORD),
-        // database: configService.get(ConfigureEnum.DATABASE_DB),
-        host: 'relaxy',
-        port: 3306,
-        username: 'relaxy',
-        password: 'relaxy',
-        database: 'relaxy',
-        entities: [__dirname + '/**/common/entities/*.entity{.ts,.js}'],
-        // synchronize: configService.get(ConfigureEnum.DATABASE_SYNCRONIZE),
-        // logging: !!configService.get(ConfigureEnum.DATABASE_LOGGING),
-        synchronize: true,
-        logging: true,
-        logger: 'file',
-      }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'relaxy',
+      password: 'relaxy',
+      database: 'relaxy',
+      entities: [__dirname + '/**/common/entities/*.entity{.ts,.js}'],
+      synchronize: true,
+      logging: true,
+      logger: 'file',
     }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-  // constructor(ConfigService: ConfigService) {
-  //   console.log(ConfigService.get(ConfigureEnum.));
-  // }
-}
+export class AppModule {}
